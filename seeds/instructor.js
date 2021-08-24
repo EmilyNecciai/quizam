40b5a079bad7517f2d05769bacbc670a86593f40
Gaye Proctor <gayebroni@yahoo.com>
2021-08-23 20:49:46 -0400

    updated file to add code for seed file

--- seeds/instructor.js
+++ seeds/instructor.js
@@ -1,1 +1,17 @@
-//placeholder seed file.
+const sequelize = require('../config/connection');
+const { Instructor, Questions } = require('../models');
+
+const instructordata = [
+  {
+    name: 'alesmonde0',
+    email: 'nwestnedge0@cbc.ca',
+    password: 'password123',
+    subject_id: 2
+  },
+{...}
+
+];
+
+const seedInstructors = () => User.bulkCreate(instructordata , {individualHooks: true});
+
+module.exports = seedInstructors;
