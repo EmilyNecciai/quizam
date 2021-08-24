73dd8133d9a3ce14fe9e58c265065b297f7d81bb
Emily N Mayeski <enmayeski@gmail.com>
2021-08-23 20:26:47 -0400

    updated routes and controllers based on new schema structure

--- models/index.js
+++ models/index.js
@@ -2,30 +2,27 @@
 const Instructor = require('./Instructor');
 const Subject = require('./Subject');
 const Question = require('./Question');
-const AnswerChoices = require('./AnswerChoices');
 
 // create associations
 Instructor.hasMany(Subject, {
-  foreignKey: 'instructor_id'
+  foreignKey: 'instructor_id',
+  onDelete: 'CASCADE'
 });
+
+Subject.hasMany(Question, {
+  foreignKey: 'subject_id',
+  onDelete: 'CASCADE'
+});
+
 
 Question.belongsToMany(Subject, {
-  foreignKey: 'question_id'
+  foreignKey: 'question_id',
+  onDelete: 'CASCADE'
 });
-
-AnswerChoices.belongsToMany(Question, {
-  foreignKey: 'answerchoices_id'
-});
-
-Question.hasMany(AnswerChoices, {
-    foreignKey: 'question_id'
-  });
-  
 
 
 module.exports = { 
     Instructor, 
     Subject, 
     Question, 
-    AnswerChoices 
 };
