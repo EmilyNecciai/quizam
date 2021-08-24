5250496cea522b4f223f89868ac9cb4dc6b96aa3
Emily N Mayeski <enmayeski@gmail.com>
2021-08-23 20:43:22 -0400

    added initial instructor routes. Possibly to change

--- controllers/index.js
+++ controllers/index.js
@@ -1,1 +1,9 @@
-// placeholder for routes center and router.use()
+const router = require('express').Router();
+
+const apiRoutes = require('./api/');
+const homeRoutes = require('./home-routes.js');
+
+router.use('/', homeRoutes);
+router.use('/api', apiRoutes);
+
+module.exports = router;
