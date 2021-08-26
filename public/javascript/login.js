26aea9c4e0a7320c19c2929a7c9fe75fbcf985a4
Emily N Mayeski <enmayeski@gmail.com>
2021-08-25 21:08:21 -0400

    added js login and logout functions

---
+++ public/javascript/login.js
@@ -0,0 +1,54 @@
+async function loginFormHandler(event) {
+    event.preventDefault();
+  
+    const email = document.querySelector('#email').value.trim();
+    const password = document.querySelector('#password').value.trim();
+  
+    if (email && password) {
+      const response = await fetch('/api/instructors/login', {
+        method: 'post',
+        body: JSON.stringify({
+          email,
+          password
+        }),
+        headers: { 'Content-Type': 'application/json' }
+      });
+  
+      if (response.ok) {
+        document.location.replace('/dashboard/');
+      } else {
+        alert(response.statusText);
+      }
+    }
+  }
+  
+  async function signupFormHandler(event) {
+    event.preventDefault();
+  
+    const username = document.querySelector('#username').value.trim();
+    const email = document.querySelector('#email').value.trim();
+    const password = document.querySelector('#password').value.trim();
+  
+    if (username && email && password) {
+      const response = await fetch('/api/instructors', {
+        method: 'post',
+        body: JSON.stringify({
+          name,
+          email,
+          password
+        }),
+        headers: { 'Content-Type': 'application/json' }
+      });
+  
+      if (response.ok) {
+        document.location.replace('/dashboard/');
+      } else {
+        alert(response.statusText);
+      }
+    }
+  }
+  
+  document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
+  
+  document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
+  
