cee73329897c5707fc342a13232e13a2e590a6be
cffhr99 <littlema@BAIYANGs-Mini.verizon.net>
2021-08-25 22:38:59 -0400

    create pdf

--- controllers/index.js
+++ controllers/index.js
@@ -2,10 +2,10 @@ const router = require('express').Router();
 
 const apiRoutes = require('./api/');
 const dashboardRoutes = require('./dashboard-routes.js');
-
+const pdfRoutes = require('./pdf-routes.js')
 
 router.use('/api', apiRoutes);
 router.use('/', dashboardRoutes);
-
+router.use('/',pdfRoutes);
 
 module.exports = router;
