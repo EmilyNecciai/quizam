2b498203b6c8548c80b1541e495d9be55a9399bc
Gaye Proctor <gayebroni@yahoo.com>
2021-08-25 20:21:35 -0400

    updated subject and questions seed files

--- seeds/questions-seeds.js
+++ seeds/questions-seeds.js
@@ -1,1 +1,38 @@
-//placeholder seed file.
+const sequelize = require('../config/connection');
+const { Questions, Subject} = require('../models');
+
+const questionsdata = [
+  {
+    id: 'Question ID??'
+    title: 'What does HTML stand for?'
+    <br>
+    'A   Hyperlinks and Text Markup Language'
+    'B   Home Tool Markup Language'
+    'C   Hyper Text Markup Language',
+    
+    correct_answer: 'ChoiceC',
+    
+    choiceA: 'A   Hyperlinks and Text Markup Language',
+    choiceB: 'B   Home Tool Markup Language',
+    choiceC: 'C   Hyper Text Markup Language',
+    choiceD: null,
+    subject_id: (HTML Course)
+  },
+
+
+  {
+    questions: 'Choose the correct HTML element for the largest heading.',
+    subject_id: ?? (HTML)
+  },
+
+  {
+    questions: 'What is the correct HTML for adding a background color?',
+    subject_id: ?? (HTML)
+  },
+
+// Test
+];
+
+const seedQuestions = () => User.bulkCreate(questionsdata , {individualHooks: true});
+
+module.exports = seedQuestions;//
--- seeds/subject-seeds.js
+++ seeds/subject-seeds.js
@@ -1,1 +1,30 @@
-//placeholder seed file.
+const sequelize = require('../config/connection');
+const { subject, Questions } = require('../models');
+
+const subjectdata = [
+  {
+    id: 'subject??'
+    subjectname: 'HTML',
+    question_id: 1??
+  },
+
+
+  {
+    id: 'subject??'
+    subjectname: 'CSS',
+    question_id: 2??
+  },
+
+  {
+    id: 'subject??'
+    subjectname: 'JavaScript',
+    question_id: 3??
+  },
+
+{...}
+// Test
+];
+
+const seedInstructors = () => User.bulkCreate(instructordata , {individualHooks: true});
+
+module.exports = seedInstructors;/placeholder seed file.
