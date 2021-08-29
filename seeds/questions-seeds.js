698d1c2b0cb0da870017114f5049f5d40c7bf675
Gaye Proctor <gayebroni@yahoo.com>
2021-08-29 13:47:29 -0400

    updated questions for CSS and JS

--- seeds/questions-seeds.js
+++ seeds/questions-seeds.js
@@ -53,10 +53,62 @@ const questionsdata = [
     choiceD: null,
     correct_answer: 'choiceA',
     subject_id:
-  }
+  },
+
+  {
+    id: '',
+    title: 'Where in an HTML document is the correct place to refer to an external style sheet?',
+    choiceA:‘In the <body> section’,
+    choiceB:'In the<head> section',
+    choiceC:'At the end of the document is',
+    choiceD: null,
+    correct_answer:'choiceB',
+    subject_id:
+  },
+
+{
+  id: '',
+  title: "Which is the correct CSS syntax? ",
+  choiceA: 'body:color=black;',
+  choiceB: '{body;color:black;',
+  choiceC: 'body {color:black;}',
+  choiceD: '{body:color=black;}'
+  correct_answer:'choiceC'
+},
+
+// JS QUIZ
+{
+    id: '',
+    title: 'Inside which HTML element do we put the JavaScript?',
+    choiceA: '<script>',
+    choiceB: '<js>',
+    choiceC: '<scripting>',
+    choiceD: '<javascript>',
+    correct_answer: 'choiceA',
+    subject_id: 
+
+},
+
+{
+  id: '',
+  title: 'To declare an array in Java, define the variable type with:',
+  choiceA: '()',
+  choiceB: '{}',
+  choiceC: '[]',
+  choiceD: null,
+  correct_answer: 'choiceC',
+  subject_id:  
+}
 
 
-
+id: '',
+    title: 'In Java, it is possible to inherit attributes and methods from one class to another.',
+    choiceA: 'False',
+    choiceB: 'True',
+    choiceC: null,
+    choiceD: null,
+    correct_answer: 'choiceA',
+    subject_id: 
 // Test
 ];
 
