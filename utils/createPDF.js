44ee05535c0a9b1f9bc0902300f9d90632aad51c
cffhr99 <littlema@BAIYANGs-Mini.verizon.net>
2021-08-30 19:47:03 -0400

    edit PDF-router

--- utils/createPDF.js
+++ utils/createPDF.js
@@ -2,9 +2,16 @@ const puppeteer = require("puppeteer");
 
 const createpdf = async (url, pdfurl) => {
   // Browser actions & buffer creator
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({
+    headless: true,
+    args: [
+      '--no-sandbox',
+      '--disable-setuid-sandbox',
+      '--disable-dev-shm-usage',
+    ],
+  });
   const page = await browser.newPage();
-  await page.goto(url);
+  await page.goto(url,{waitUntil: 'networkidle0'});
   const pdf = await page.pdf({
     format: "letter",
     path: pdfurl,
