3a9b97122ee68a4b2129957acad871c473154268
cffhr99 <littlema@BAIYANGs-Mini.verizon.net>
2021-09-02 02:26:52 -0400

    edited auth.js

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -4,9 +4,10 @@ const withAuth = require("../utils/auth");
 
 // get all Subjects
 router.get("/",withAuth, (req, res) => {
+  console.log(req.session)
   Subject.findAll({
     where: {
-      user_id: req.session.user_id,
+      id: req.session.id,
     },
     attributes: ["id", "name"],
     include: [
@@ -30,8 +31,8 @@ router.get("/",withAuth, (req, res) => {
     ],
   })
     .then((dbSubjectData) => {
-      const subject = dbSubjectData.map(subject => subject.get({plain:true}));
-      res.render('dashboard',{subject, loggedIn: false});
+      const subjects = dbSubjectData.map(subject => subject.get({plain:true}));
+      res.render('dashboard',{subjects, loggedIn: true});
       // res.json(dbSubjectData)
     })
     .catch((err) => {
@@ -40,7 +41,7 @@ router.get("/",withAuth, (req, res) => {
     });
 });
 
-//Find Specific Subject
+// Find Specific Subject
 router.get("/:id", withAuth, (req, res) => {
   Subject.findOne({
     where: {
@@ -85,75 +86,75 @@ router.get("/:id", withAuth, (req, res) => {
 
 
 // get all Questions
-router.get("/", withAuth, (req, res) => {
-  Question.findAll({
-    attributes: [
-      "id",
-      "title",
-      "correct_answer",
-      "choiceA",
-      "choiceB",
-      "choiceC",
-      "choiceD",
-      "subject_id",
-    ],
-    include: [
-      {
-        model: Instructor,
-        attributes: ["id", "name", "email"],
-      },
-      {
-        model: Subject,
-        attributes: ["id", "name"],
-      },
-    ],
-  })
-    .then((dbQuestionData) => res.json(dbQuestionData))
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json(err);
-    });
-});
+// router.get("/", withAuth, (req, res) => {
+//   Question.findAll({
+//     attributes: [
+//       "id",
+//       "title",
+//       "correct_answer",
+//       "choiceA",
+//       "choiceB",
+//       "choiceC",
+//       "choiceD",
+//       "subject_id",
+//     ],
+//     include: [
+//       {
+//         model: Instructor,
+//         attributes: ["id", "name", "email"],
+//       },
+//       {
+//         model: Subject,
+//         attributes: ["id", "name"],
+//       },
+//     ],
+//   })
+//     .then((dbQuestionData) => res.json(dbQuestionData))
+//     .catch((err) => {
+//       console.log(err);
+//       res.status(500).json(err);
+//     });
+// });
 
-//Find Specific Question
-router.get("/:id", withAuth, (req, res) => {
-  Question.findOne({
-    where: {
-      id: req.params.id,
-    },
-    attributes: [
-      "id",
-      "title",
-      "correct_answer",
-      "choiceA",
-      "choiceB",
-      "choiceC",
-      "choiceD",
-      "subject_id",
-    ],
-    include: [
-      {
-        model: Instructor,
-        attributes: ["id", "name", "email"],
-      },
-      {
-        model: Subject,
-        attributes: ["id", "name"],
-      },
-    ],
-  })
-    .then((dbQuestionData) => {
-      if (!dbQuestionData) {
-        res.status(404).json({ message: "No question found with this id" });
-        return;
-      }
-      res.json(dbQuestionData);
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json(err);
-    });
-});
+// //Find Specific Question
+// router.get("/:id", withAuth, (req, res) => {
+//   Question.findOne({
+//     where: {
+//       id: req.params.id,
+//     },
+//     attributes: [
+//       "id",
+//       "title",
+//       "correct_answer",
+//       "choiceA",
+//       "choiceB",
+//       "choiceC",
+//       "choiceD",
+//       "subject_id",
+//     ],
+//     include: [
+//       {
+//         model: Instructor,
+//         attributes: ["id", "name", "email"],
+//       },
+//       {
+//         model: Subject,
+//         attributes: ["id", "name"],
+//       },
+//     ],
+//   })
+//     .then((dbQuestionData) => {
+//       if (!dbQuestionData) {
+//         res.status(404).json({ message: "No question found with this id" });
+//         return;
+//       }
+//       res.json(dbQuestionData);
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//       res.status(500).json(err);
+//     });
+// });
 
 module.exports = router;
 
--- utils/auth.js
+++ utils/auth.js
@@ -1,5 +1,5 @@
 const withAuth = (req, res, next) => {
-  if (!req.session.user_id) {
+  if (!req.session.id) {
     res.redirect("/login");
   } else {
     next();
