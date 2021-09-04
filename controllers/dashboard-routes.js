e462c6e39fab15d8c214d97d79901225f36c8416
cffhr99 <littlema@BAIYANGs-Mini.verizon.net>
2021-09-04 03:22:07 -0400

    add-question, view question and subject, export PDF

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const { Instructor, Subject, Question } = require("../models");
 const withAuth = require("../utils/auth");
+const sequelize = require('../config/connection');
 
 // get all Subjects
 router.get("/",withAuth, (req, res) => {
@@ -65,7 +66,7 @@ router.get("/subjects",withAuth, (req, res) => {
           "choiceB",
           "choiceC",
           "choiceD",
-          "subject_id",
+          "subject_id"
         ],
       },
     ],
@@ -118,6 +119,115 @@ router.get('/addsubject',withAuth,(req,res)=>{
     res.status(500).json(err);
   });
 });
+
+
+router.get('/addquestion',withAuth,(req,res)=>{
+  Subject.findAll({
+    where:{
+      instructor_id:req.session.user_id
+    },
+    attributes:[
+      'id',
+      'name',
+      'created_at'
+    ],
+    include:[
+      {
+        model:Question,
+        attributes: [
+          "id",
+          "title",
+          "correct_answer",
+          "choiceA",
+          "choiceB",
+          "choiceC",
+          "choiceD",
+          "subject_id"
+        ],
+      },{
+        model:Instructor,
+        attributes:['name']
+      }
+    ]
+  })
+  .then(dbSubjectData => {
+    const subjects = dbSubjectData.map(subject => subject.get({ plain: true }));
+    res.render('add-question', {subjects, loggedIn: true });
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
+});
+
+router.get("/questions",withAuth, (req, res) => {
+  console.log(req.session)
+  Question.findAll({
+    attributes: [
+      "id",
+      "title",
+      "correct_answer",
+      "choiceA",
+      "choiceB",
+      "choiceC",
+      "choiceD",
+      "subject_id",
+      [sequelize.literal('(SELECT name FROM subjects WHERE subjects.id = questions.subject_id)'), 'subject_name']
+    ],
+    include: [
+      {
+        model: Subject,
+        attributes: [
+          "id",
+          "name"
+        ],
+      },
+    ],
+  })
+    .then((dbQuestionData) => {
+      const questions = dbQuestionData.map(question => question.get({plain:true}));
+      res.render('questions',{questions, loggedIn: true});
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.get("/answers",withAuth, (req, res) => {
+  console.log(req.session)
+  Question.findAll({
+    attributes: [
+      "id",
+      "title",
+      "correct_answer",
+      "choiceA",
+      "choiceB",
+      "choiceC",
+      "choiceD",
+      "subject_id",
+      [sequelize.literal('(SELECT name FROM subjects WHERE subjects.id = questions.subject_id)'), 'subject_name']
+    ],
+    include: [
+      {
+        model: Subject,
+        attributes: [
+          "id",
+          "name"
+        ],
+      },
+    ],
+  })
+    .then((dbQuestionData) => {
+      const questions = dbQuestionData.map(question => question.get({plain:true}));
+      res.render('answers',{questions, loggedIn: true});
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
 
 module.exports = router;
 
